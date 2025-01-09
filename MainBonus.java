b9bcdb68b315ddc830e11bfbc7a77cf3449a3b28
Romario QUASHIE <QUASHIE_Ro@lprs.local>
2025-01-09 15:38:51 +0100

    feat(MainBonus.java) : -création d'une liste (recette) contenant les ingrédients de type objets -manipulation de la liste avec des methodes (add,remove) ou des methodes de la classe Ingredient pour ajouter les ingredients ou modifier les quantites

--- MainBonus.java
+++ MainBonus.java
@@ -1,2 +1,50 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
 public class MainBonus {
+    public static void main(String[] args) {
+        ArrayList <Ingredient> recette = new ArrayList<>();
+
+        //créer un nouvel ingrédient
+        Ingredient jambon = new Ingredient("Jambon",4,"tranches") ;
+        recette.add(jambon) ;
+        Ingredient emmental = new Ingredient("Emmental",8,"tranches") ;
+        recette.add(emmental) ;
+
+        //Afficher l'ensemble des ingredients
+        System.out.println("\n---------Afficehr l'enssemble des ingredients---------");
+        for(Ingredient i : recette) {
+          System.out.println(i.afficher()) ;
+        }
+        //Afficher un seul ingredient à la position
+        System.out.println("-------Afficher un ingredient-------------");
+        System.out.println("Le 2ème ingrédient : "+recette.get(1).afficher()) ;
+        System.out.println("\n------------Remplacer un ingredient----------------------");
+        //remplacer un ingredient de notre liste
+        System.out.println("Ingrédient initial : "+recette.get(1).afficher()) ;
+        Ingredient ail = new Ingredient("Ail",1,"gousse") ;
+        recette.add(ail) ;
+        recette.set(1,ail);
+        System.out.println("Nouvel ingredient : "+recette.get(1).afficher()) ;
+
+        //Supprimer un ingredient de notre Arraylist
+        System.out.println("\n--------Supprimer un ingredient-------------------");
+        System.out.println("Supprimer l'ail  ");
+        recette.remove(ail) ;
+        boolean supprimer = recette.remove(ail) ;
+        System.out.println("Ail supprimé : "+supprimer);
+        int numIngredient = 0 ;
+        for(Ingredient i : recette) {
+            numIngredient++ ;
+            System.out.println("Ingredient "+numIngredient +" : ");
+            System.out.println(i.afficher()) ;
+        }
+        System.out.println("\n-------------Modifier la quantite-------------------");
+        //Modifier la quantité
+       System.out.println("Quantité initial : "+jambon.getQuantite()) ;
+       int qteJambon = jambon.setQuantite(2) ;
+        System.out.println("Nouvelle quantité : "+qteJambon);
+
+    }
 }
